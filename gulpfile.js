707b8b690e43c09bac3c2f492151799bac4ee69c
Yan <yan-zakharov-03@mail.ru>
2022-09-18 15:36:07 +0700

    Правки автоматицазии

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
-import {deleteAsync} from 'del';
+import del from 'del';
 
 // Styles
 
@@ -102,7 +102,7 @@ const copy = (done) => {
 // Clean
 
 const clean = () => {
-  return deleteAsync('build');
+  return del('build');
 }
 
 // Server

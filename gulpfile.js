31291cb81a0c1d26307526b88fa775b39c783ad5
Yan <yan-zakharov-03@mail.ru>
2022-09-26 20:56:29 +0700

    1

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import csso from 'postcss-csso';
-import csso from 'postcss-csso';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import rename from 'gulp-rename';
@@ -12,7 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
-import del from 'del';
+import {deleteAsync} from 'del';
 // Styles
 export const styles = () => {
   return gulp.src('source/less/style.less', { sourcemaps: true })
@@ -20,7 +19,6 @@ export const styles = () => {
     .pipe(less())
     .pipe(postcss([
       autoprefixer(),
-      csso(),
       csso()
     ]))
     .pipe(rename('style.min.css'))
@@ -86,7 +84,7 @@ const copy = (done) => {
 }
 // Clean
 const clean = () => {
-  return del('build');
+  return deleteAsync('build');
 }
 // Server
 const server = (done) => {
@@ -108,8 +106,6 @@ const reload = (done) => {
 // Watcher
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
-  // gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html', gulp.series(html, reload));
   gulp.watch('source/js/script.js', gulp.series(scripts));
   gulp.watch('source/*.html', gulp.series(html, reload));
 }
@@ -133,10 +129,6 @@ export default gulp.series(
   copy,
   copyImages,
   gulp.parallel(
-    clean,
-  copy,
-  copyImages,
-  gulp.parallel(
     styles,
     html,
     scripts,
@@ -145,15 +137,6 @@ export default gulp.series(
     createWebp
   ),
   gulp.series(
-   
-    html,
-    scripts,
-    svg,
-    sprite,
-    createWebp
-  ),
-  gulp.series(
     server,
-   
     watcher
-  )  ));
+  ));
